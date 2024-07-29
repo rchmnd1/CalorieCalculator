f1117c7f1da3236fd4a11bb2cd31b45959ba6b81
rchmnd1 <23007721@myrp.edu.sg>
2024-07-29 10:40:44 +0800

    const PORT = process.env.PORT || 3306;

--- app.js
+++ app.js
@@ -311,5 +311,5 @@ app.get('/deletefood/:id', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
